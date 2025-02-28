9102c6a0ccd1292c4fac369759b408702960fdce
connie <181677440+rvl-005@users.noreply.github.com>
2025-02-28 18:47:51 -0500

    added Kelvin

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,33 @@
-from temperature import Celsius as cel, Fahrenheit as fah
+from temperature import Celsius as cel, Fahrenheit as fah, Kelvin as kel
 
+try:
+    tempInput = float(input("Enter the temperature you want to convert: "))
+    scaleInput = input("Enter the scale of the temperature you entered (Celsius, Fahrenheit, or Kelvin): ")
+    newScale = input("Enter the scale you want to convert the temperature to (Celsius, Fahrenheit, or Kelvin): ")
 
-tempInput = float(input("Enter the temperature you want to convert: "))
-scaleInput = input("Enter the scale of the temperature you entered (Celsius/Fahrenheit): ")
+    def ConvertTemperature(tempInput, scaleInput, newScale):
+        if scaleInput == "Celsius":
+            if newScale == "Fahrenheit":
+                return cel(tempInput).convertFahrenheit()
+            elif newScale == "Kelvin":
+                return cel(tempInput).convertKelvin()
+        elif scaleInput == "Fahrenheit":
+            if newScale == "Celsius":
+                return fah(tempInput).convertCelsius()
+            elif newScale == "Kelvin":
+                return fah(tempInput).convertKelvin()
+            return fah(tempInput).convertCelsius()
+        elif scaleInput == "Kelvin":
+            if newScale == "Celsius":
+                return kel(tempInput).convertCelsius()
+            elif newScale == "Fahrenheit":
+                return kel(tempInput).convertFahrenheit()
 
-def ConvertTemperature(tempInput, scaleInput):
-    if scaleInput == "Celsius":
-        return cel(tempInput).convertFahrenheit()
-    elif scaleInput == "Fahrenheit":
-        return fah(tempInput).convertCelsius()
-    else:
-        return "Invalid scale"
-newTemp = ConvertTemperature(tempInput, scaleInput)
-if newTemp.temp.is_integer() == True:
-    newTemp.temp = int(newTemp.temp)
-print(f"Temperature in {newTemp.scale}: {newTemp}")
+    newTemp = ConvertTemperature(tempInput, scaleInput, newScale)
+    if newTemp.temp.is_integer() == True:
+        newTemp.temp = int(newTemp.temp)
+    print(f"Temperature in {newScale}: {newTemp}")
+except ValueError:
+    print("Invalid input. Please enter a number for the temperature.")
+except AttributeError:
+    print("Invalid scale. This scale supports Celsius, Fahrenheit, and Kelvin. Please try again.")
--- app/temperature.py
+++ app/temperature.py
@@ -16,6 +16,9 @@ class Celsius(Temperature):
     def convertFahrenheit(self):
         return Fahrenheit((self.temp * 1.8) + 32)
     
+    def convertKelvin(self):
+        return Kelvin(self.temp + 273.15)
+    
 class Fahrenheit(Temperature):
     def __init__(self, temp):
         super().__init__(temp, "F")
@@ -25,3 +28,19 @@ class Fahrenheit(Temperature):
     
     def convertCelsius(self):
         return Celsius((self.temp - 32) / 1.8)
+    
+    def convertKelvin(self):
+        return Kelvin((self.temp + 459.67) * 5/9)
+    
+class Kelvin(Temperature):
+    def __init__(self, temp):
+        super().__init__(temp, "K")
+        
+    def __str__(self):
+        return f"{self.temp} {self.scale}"
+    
+    def convertCelsius(self):
+        return Celsius(self.temp - 273.15)
+    
+    def convertFahrenheit(self):
+        return Fahrenheit((self.temp * 9/5) - 459.67)
